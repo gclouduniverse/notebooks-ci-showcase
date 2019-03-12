1b9bf43089ef5ab53d7e1abebcf43a935afd0524
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2019-03-12 14:32:57 -0700

    update cloudbuild pipline

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+wget https://github.com/gclouduniverse/JupyterNotebooksDevelopmentManifesto/blob/master/3_self_contained/enable_notebook_submission.sh .
+chmod +x enable_notebook_submission.sh
+./enable_notebook_submission.sh
+
 execute_notebook_with_gpu basic_text_classification.ipynb gs://dl-platform-temp/notebook-ci-showcase p100 1 || exit 1
 if [[ ! -f ./notebook.ipynb ]]; then
    exit 1
