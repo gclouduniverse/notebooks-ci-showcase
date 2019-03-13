f190f34fcaf7f3a8c494f59e0ce1d89b2e7a6acf
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2019-03-12 18:17:23 -0700

    Working version of the test

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM alpine
-COPY run_test.sh /
-CMD ["/run_test.sh"]
--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,77 @@
 #!/bin/bash
+readonly DEFAULT_NOTEBOOK_EXECUTOR_INSTANCE_NAME="notebookexecutor"
 
-wget https://github.com/gclouduniverse/JupyterNotebooksDevelopmentManifesto/blob/master/3_self_contained/enable_notebook_submission.sh .
-chmod +x enable_notebook_submission.sh
-./enable_notebook_submission.sh
+function wait_till_instance_not_exist() {
+    INSTANCE_NAME=$1
+    ZONE=$2
+    if [ "$#" -ne 2 ]; then
+        echo "Usage: "
+        echo "   ./wait_till_instance_not_exist [INSTANCE_NAME] [ZONE]"
+        echo ""
+        echo "example:"
+        echo "   ./wait_till_instance_not_exist instance1 us-west1-b"
+        echo ""
+        return 1
+    fi
+    while true; do
+        if gcloud compute instances describe "${INSTANCE_NAME}" --zone="${ZONE}" > /dev/null; then
+            echo "Background execution still in progress, going to sleep 10sec..."
+            sleep 10
+            continue
+        fi
+        return 0
+    done
+}
+
+function execute_notebook_with_gpu() {
+    if [ "$#" -ne 4 ]; then
+        echo "Usage: "
+        echo "   ./execute_notebook_with_gpu [INPUT_NOTEBOOK] [GCS_TEMP_LOCATION] [GPU_TYPE] [GPU_COUNT]"
+        echo ""
+        echo "example:"
+        echo "   ./execute_notebook_with_gpu test.ipynb gs://my-bucket p100 4"
+        echo ""
+        return 1
+    fi 
+    IMAGE_FAMILY="tf-latest-cu100" # or put any required
+    ZONE="us-west1-b"
+    INSTANCE_NAME="${DEFAULT_NOTEBOOK_EXECUTOR_INSTANCE_NAME}"
+    INSTANCE_TYPE="n1-standard-8"
+    INPUT_NOTEBOOK=$1
+    TEMP_GCS_LOCATION=$2
+    INPUT_NOTEBOOK_GCS_PATH="${TEMP_GCS_LOCATION}/${INPUT_NOTEBOOK}"
+    GPU_TYPE=$3
+    GPU_COUNT=$4
+    gsutil cp "./${INPUT_NOTEBOOK}" "${TEMP_GCS_LOCATION}/"
+    if [[ $? -eq 1 ]]; then
+        echo "Upload to the temp GCS location (${TEMP_GCS_LOCATION}) of the notebook (./${INPUT_NOTEBOOK}) have failed."
+        return 1
+    fi
+    gcloud compute instances create "${INSTANCE_NAME}" \
+            --zone="${ZONE}" \
+            --image-family="${IMAGE_FAMILY}" \
+            --image-project=deeplearning-platform-release \
+            --maintenance-policy=TERMINATE \
+            --accelerator="type=nvidia-tesla-${GPU_TYPE},count=${GPU_COUNT}" \
+            --machine-type="${INSTANCE_TYPE}" \
+            --boot-disk-size=200GB \
+            --scopes=https://www.googleapis.com/auth/cloud-platform \
+            --metadata="input_notebook=${INPUT_NOTEBOOK_GCS_PATH},output_notebook=${TEMP_GCS_LOCATION},startup-script-url=https://raw.githubusercontent.com/b0noI/gcp-notebook-executor/master/notebook_executor.sh" \
+            --quiet
+    if [[ $? -eq 1 ]]; then
+        echo "Creation of background instance for trainin have failed."
+        return 1
+    fi
+    wait_till_instance_not_exist "${INSTANCE_NAME}" "${ZONE}"
+    echo "execution has been finished, downloading the result (notebook.ipynb)"
+    gsutil cp "${TEMP_GCS_LOCATION}/notebook.ipynb" .
+    if [[ $? -eq 1 ]]; then
+        echo "There was a problem with downloading result notebook (${TEMP_GCS_LOCATION}/notebook.ipynb)."
+        return 1
+    fi
+    echo "done"
+    return 0
+}
 
 execute_notebook_with_gpu basic_text_classification.ipynb gs://dl-platform-temp/notebook-ci-showcase p100 1 || exit 1
 if [[ ! -f ./notebook.ipynb ]]; then
