fc6792248b0de654d82a8e2a7bccd1ca1feb178c
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2019-04-06 13:29:37 -0700

    final migration of the execution logic to the official gcp-notebook-executor

--- run_notebook_instance.sh
+++ run_notebook_instance.sh
@@ -41,66 +41,16 @@ function output_for_mode() {
     fi
 }
 
-function execute_notebook_with_gpu() {
-    if [ "$#" -ne 4 ]; then
-        echo "Usage: "
-        echo "   ./execute_notebook_with_gpu [INPUT_NOTEBOOK] [GCS_LOCATION] [GPU_TYPE] [GPU_COUNT]"
-        echo ""
-        echo "example:"
-        echo "   ./execute_notebook_with_gpu test.ipynb gs://my-bucket p100 4"
-        echo ""
-        return 1
-    fi 
-    echo "Build id: ${BUILD_TIME}"
-    local INPUT_NOTEBOOK=$1
-    local GCS_LOCATION=$2
-    local GPU_TYPE=$3
-    local GPU_COUNT=$4
-    NOTEBOOK_NAME=$(basename ${INPUT_NOTEBOOK})
-    INPUT_NOTEBOOK_GCS_PATH="${GCS_LOCATION}/staging/${BUILD_TIME}/${NOTEBOOK_NAME}"
-    if [[ ! -z ${PARAM_FILE} ]]; then
-        INPUT_PARAM_GCS_PATH="${GCS_LOCATION}/staging/${BUILD_TIME}/params.yaml"
-        gsutil cp "${PARAM_FILE}" "${INPUT_PARAM_GCS_PATH}"
-        PARAM_METADATA=",parameters_file=${INPUT_PARAM_GCS_PATH}"
-    fi
-    OUTPUT_NOTEBOOK_GCS_FOLDER=$(output_for_mode "${TESTING_MODE}" "${GCS_LOCATION}" "${OUTPUT_DATE}")
-    OUTPUT_NOTEBOOK_GCS_PATH="${OUTPUT_NOTEBOOK_GCS_FOLDER}/${NOTEBOOK_NAME}"
-    echo "Staging notebook: ${INPUT_NOTEBOOK_GCS_PATH}"
-    echo "Output notebook: ${OUTPUT_NOTEBOOK_GCS_PATH}"
-    gsutil cp "${INPUT_NOTEBOOK}" "${INPUT_NOTEBOOK_GCS_PATH}"
-    if [[ $? -eq 1 ]]; then
-        echo "Upload to the temp GCS location (${INPUT_NOTEBOOK_GCS_PATH}) of the notebook (${INPUT_NOTEBOOK}) has failed."
-        return 1
-    fi
-    IMAGE_FAMILY="tf-latest-cu100" # or put any required
-    ZONE="us-west1-b"
-    INSTANCE_NAME="${DEFAULT_NOTEBOOK_EXECUTOR_INSTANCE_NAME}"
-    INSTANCE_TYPE="n1-standard-8"
-    gcloud compute instances create "${INSTANCE_NAME}" \
-            --zone="${ZONE}" \
-            --image-family="${IMAGE_FAMILY}" \
-            --image-project=deeplearning-platform-release \
-            --maintenance-policy=TERMINATE \
-            --accelerator="type=nvidia-tesla-${GPU_TYPE},count=${GPU_COUNT}" \
-            --machine-type="${INSTANCE_TYPE}" \
-            --boot-disk-size=200GB \
-            --scopes=https://www.googleapis.com/auth/cloud-platform \
-            --metadata="api_key=${API_KEY},input_notebook=${INPUT_NOTEBOOK_GCS_PATH},output_notebook=${OUTPUT_NOTEBOOK_GCS_FOLDER}${PARAM_METADATA:-}" \
-            --metadata-from-file="startup-script=gcp-notebook-executor/notebook_executor.sh" \
-            --quiet
-    if [[ $? -eq 1 ]]; then
-        echo "Creation of background instance for training has failed."
-        return 1
-    fi
-    wait_till_instance_not_exist "${INSTANCE_NAME}" "${ZONE}"
-    echo "execution has been finished, checking result"
-    OUTPUT_CONTENTS=$(gsutil ls "${OUTPUT_NOTEBOOK_GCS_FOLDER}")
-    if [[ $? -ne 0 ]] || grep -q "FAILED" <<< "${OUTPUT_CONTENTS}"; then
-        echo "Job failed or unable to get output."
-        return 1
-    fi
-    echo "done"
-    return 0
-}
-
-execute_notebook_with_gpu demo.ipynb gs://dl-platform-temp/notebook-ci-showcase p100 1
+INPUT_NOTEBOOK="demo.ipynb"
+NOTEBOOK_NAME=$(basename ${INPUT_NOTEBOOK})
+GCS_LOCATION="gs://dl-platform-temp/notebook-ci-showcase"
+INPUT_NOTEBOOK_GCS_PATH="${GCS_LOCATION}/staging/${BUILD_TIME}/${NOTEBOOK_NAME}"
+PARAM_METADATA=""
+if [[ ! -z ${PARAM_FILE} ]]; then
+    INPUT_PARAM_GCS_PATH="${GCS_LOCATION}/staging/${BUILD_TIME}/params.yaml"
+    gsutil cp "${PARAM_FILE}" "${INPUT_PARAM_GCS_PATH}"
+    PARAM_METADATA=",parameters_file=${INPUT_PARAM_GCS_PATH}"
+fi
+OUTPUT_NOTEBOOK_GCS_FOLDER=$(output_for_mode "${TESTING_MODE}" "${GCS_LOCATION}" "${OUTPUT_DATE}")
+OUTPUT_NOTEBOOK_GCS_PATH="${OUTPUT_NOTEBOOK_GCS_FOLDER}/${NOTEBOOK_NAME}"
+execute_notebook -i "./${INPUT_NOTEBOOK}" -o "${INPUT_NOTEBOOK_GCS_PATH}" -m "api_key=${API_KEY}${PARAM_METADATA}" -g t4 -c 1
