004d76775d49d9e70c9458e6db8917a1f32fb2de
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2019-04-06 13:50:44 -0700

    reverting :- substituiton logic

--- run_notebook_instance.sh
+++ run_notebook_instance.sh
@@ -54,5 +54,5 @@ fi
 OUTPUT_NOTEBOOK_GCS_FOLDER=$(output_for_mode "${TESTING_MODE}" "${GCS_LOCATION}" "${OUTPUT_DATE}")
 OUTPUT_NOTEBOOK_GCS_PATH="${OUTPUT_NOTEBOOK_GCS_FOLDER}/${NOTEBOOK_NAME}"
 echo "Going to execute the following command"
-echo "execute_notebook -i ./${INPUT_NOTEBOOK} -o ${INPUT_NOTEBOOK_GCS_PATH} -m api_key=${API_KEY}${PARAM_METADATA} -g t4 -c 1"
-execute_notebook -i "./${INPUT_NOTEBOOK}" -o "${INPUT_NOTEBOOK_GCS_PATH}" -m "api_key=${API_KEY}${PARAM_METADATA}" -g t4 -c 1
+echo "execute_notebook -i ./${INPUT_NOTEBOOK} -o ${INPUT_NOTEBOOK_GCS_PATH} -m api_key=${API_KEY}${PARAM_METADATA:-} -g t4 -c 1"
+execute_notebook -i "./${INPUT_NOTEBOOK}" -o "${INPUT_NOTEBOOK_GCS_PATH}" -m "api_key=${API_KEY}${PARAM_METADATA:-}" -g t4 -c 1
